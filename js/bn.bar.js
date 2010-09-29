47f78515860c2a6da91bf9a4efe1f29982892da6
Filipe Araujo <filipe.alves.araujo@gmail.com>
2010-09-29 15:17:19 -0400

    uploading assets

---
+++ js/bn.bar.js
@@ -0,0 +1,33 @@
+/*
+* @file CN.Iris.js
+* @author Filipe Araujo Paul Bronshteyn
+* @copyright (c) Conde Nast Digital
+*/
+
+if (typeof BN === 'undefined' || !BN) {
+    var BN = {};
+}
+
+
+/**
+ *
+* CN iris
+* @class                    CN Iris
+* @description
+* @public
+* @author					Filipe Araujo & Paul Bronshteyn
+* @requires					CN
+*/
+BN.iris = (function($){
+	var
+	init = function(){
+		
+	};
+
+	$(function(){
+		init();
+	});
+
+	return {}
+
+})(jQuery);
